129694c31d1a1be74b20ec3b373fd276a49760cf
hugo.brendow <hugobrendow97gmail.com>
2018-04-28 16:52:42 -0300

    Adicionando página de sessões

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -2,12 +2,36 @@ package br.com.caelum.ingresso.model;
 
 import java.time.LocalTime;
 
-public class Sessao {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+public class Sessao {
+	
+	@Id
+	@GeneratedValue
 	private Integer id;
+	
+	@DateTimeFormat(pattern="HH:mm")
 	private LocalTime horario;
+	
+	@ManyToOne
 	private Sala sala;
+	
+	@ManyToOne
 	private Filme filme;
+	
+	/**
+	 * @deprecated hibernate only
+	 */
+	
+	public Sessao(){
+		
+	}
 	
 	public Sessao(LocalTime horario, Filme filme, Sala sala) {
 		this.horario = horario;
